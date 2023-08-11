40d86f3c73e6ba811ac73fa1dffdf5c5a3db73be
haidang <haidang02032003@gmail.com>
2023-08-11 23:25:39 +0700

    push

--- server/src/base/base.repository.ts
+++ server/src/base/base.repository.ts
@@ -2,7 +2,6 @@ import { Model, FindOptions } from 'sequelize';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
 Injectable()
 export class BaseRepository<T extends Model> {
   constructor(private readonly model: T) {}
